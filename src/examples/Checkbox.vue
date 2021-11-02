176c5c5ff2ea648a67c7cd35d14d17722ccb5638
张海斌 <zhang_h_bin@sina.com>
2021-11-02 19:11:23 +0800

    Upd: save

--- src/examples/Checkbox.vue
+++ src/examples/Checkbox.vue
@@ -1,7 +1,14 @@
 <template>
   <section class="demo-group">
     <div class="item-group">
-      <DeCheckbox v-model="a" />
+      <DeCheckbox
+        v-model="b"
+        html-type="radio"
+        label="苹果"
+        value="apple"
+        radio-optional
+      />
+      <DeCheckbox html-type="checkbox" label="香蕉" value="banana" />
     </div>
   </section>
 </template>
@@ -16,8 +23,8 @@ export default {
   setup() {
     return {
       a: ref([]),
-      b: ref(true),
-      c: ref(false),
+      b: ref('apple'),
+      c: ref(''),
     };
   },
 };
--- src/packages/checkbox/index.vue
+++ src/packages/checkbox/index.vue
@@ -1,44 +1,111 @@
 <template>
-  <label>
-    <slot></slot>
-  </label>
+  <div :class="wrapClassList" @click="onChange">
+    <span :class="iconClassList">
+      <i class="de-checkbox__icon-inner"></i>
+    </span>
+    <slot>{{ label }}</slot>
+  </div>
 </template>
 
 <script lang="ts">
 import './index.less';
-// import {ref} from 'vue';
+import {defineComponent, ref, computed, watchEffect} from 'vue';
 
 const name = 'de-checkbox';
-export default {
+export default defineComponent({
   name,
   props: {
-    modelValue: Boolean,
-    name: {
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: '',
+    },
+    label: {
       type: String,
       default: '',
     },
     value: {
-      type: [String, Number],
-      default: '',
+      type: [String, Number, Boolean],
+      required: true,
     },
-    type: {
+    name: {
       type: String,
-      default: 'default',
-      validator: (v: string) => ['default'].includes(v) || !!v,
+      default: '',
     },
     htmlType: {
       type: String,
       default: 'checkbox',
       validator: (v: string) => ['checkbox', 'radio'].includes(v),
     },
+    falseValue: {
+      type: [String, Number, Boolean],
+      default: undefined,
+    },
+    radioOptional: Boolean,
     indeterminate: Boolean,
     disabled: Boolean,
   },
-  emits: ['update:modelValue', 'onChange', 'onClick'],
-  setup() {
-    return {};
-  },
-};
-</script>
+  emits: ['update:modelValue', 'onChange', 'onBeforeChange'],
+  setup(props, {emit}) {
+    const value = ref<boolean | string | number>('');
+    const isChecked = ref(false);
+    const isRadio = computed(() => props.htmlType === 'radio');
+    const wrapClassList = computed(() => {
+      return [
+        name,
+        {
+          [`${name}__disabled`]: props.disabled,
+          [`${name}__checked`]: isChecked.value,
+        },
+      ];
+    });
+    const iconClassList = computed(() => {
+      const _name = `${name}__icon`;
+      return [
+        _name,
+        {
+          [`${_name}-radio`]: isRadio.value,
+          [`${_name}-checkbox`]: !isRadio.value,
+        },
+      ];
+    });
 
-<style scoped></style>
+    const update = () => {
+      if (isRadio.value && isChecked.value && !props.radioOptional) return;
+      isChecked.value = !isChecked.value;
+      emit('onBeforeChange', value.value);
+      value.value = isChecked.value
+        ? props.value
+        : props.falseValue || getDefVal(props.value);
+      emit('onChange', {
+        checked: isChecked.value,
+        value: value.value,
+      });
+    };
+
+    watchEffect(() => {
+      isChecked.value = props.modelValue === props.value;
+    });
+
+    return {
+      isChecked,
+      wrapClassList,
+      iconClassList,
+      onChange() {
+        if (props.disabled) return;
+        update();
+        emit('update:modelValue', value.value);
+      },
+    };
+  },
+});
+
+function getDefVal(value: number | string | boolean) {
+  switch (typeof value) {
+    case 'number':
+    case 'string':
+      return '';
+    case 'boolean':
+      return false;
+  }
+}
+</script>
